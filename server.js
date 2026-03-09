66831f5bf6b6997c5d92d0bdbcffd583615d8ff1
T14g <ottiagos@gmail.com>
2026-03-09 18:38:16 -0300

    Dynamic template using a placeholder

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // __dirname is a global variable that contains the directory name of the current module,
+  //  in this case, the directory where server.js is located.
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  var message = 'Hello city';
+  // what is this { Message } ? It is a placeholder in the HTML file that we will replace with the actual message.
+  html = html.replace('{Message}', message);
+  res.end(html);  
 
 }).listen(1337, '127.0.0.1');
